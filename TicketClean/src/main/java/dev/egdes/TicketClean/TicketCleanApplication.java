d3e1858a6173304997db746773ce06d43e5ec257
egdes <egpelizaro@gmail.com>
2026-01-26 21:29:05 -0300

    Initial commit

---
+++ TicketClean/src/main/java/dev/egdes/TicketClean/TicketCleanApplication.java
@@ -0,0 +1,13 @@
+package dev.egdes.TicketClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketCleanApplication.class, args);
+	}
+
+}
---
+++ TicketClean/src/test/java/dev/egdes/TicketClean/TicketCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.egdes.TicketClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
